ca7feb5373603cc71f42116ddfe17701bce6fd0a
Luispe <38361000+lpbonomi@users.noreply.github.com>
2024-06-05 16:27:48 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,17 +3,17 @@ import Video from 'next-video';
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <Video src='https://ultradrop-production.s3.amazonaws.com/NTVG_Navidad_V4_ef90e288ca.MP4' />
+        <Video src='https://next-videos-ultradrop.s3.us-east-1.amazonaws.com/videos/0hOSpzin-U7fDiifE0H-hnuZ4u_V_tofCJbxpqpNJCA' />
         <Video >
-          <source src='https://ultradrop-production.s3.amazonaws.com/NTVG_Navidad_V4_ef90e288ca.MP4' type='video/mp4' />
+          <source src='https://next-videos-ultradrop.s3.us-east-1.amazonaws.com/videos/0hOSpzin-U7fDiifE0H-hnuZ4u_V_tofCJbxpqpNJCA' type='video/mp4' />
         </Video>
         <video
         controls
-        src='https://ultradrop-production.s3.amazonaws.com/NTVG_Navidad_V4_ef90e288ca.MP4' ></video>
+        src='https://next-videos-ultradrop.s3.us-east-1.amazonaws.com/videos/0hOSpzin-U7fDiifE0H-hnuZ4u_V_tofCJbxpqpNJCA'></video>
         <video
         controls
         >
-          <source src='https://ultradrop-production.s3.amazonaws.com/NTVG_Navidad_V4_ef90e288ca.MP4' type='video/mp4' />
+          <source src='https://next-videos-ultradrop.s3.us-east-1.amazonaws.com/videos/0hOSpzin-U7fDiifE0H-hnuZ4u_V_tofCJbxpqpNJCA' type='video/mp4' />
         </video>
     </main>
   );
